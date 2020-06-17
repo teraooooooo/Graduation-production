7ff2762772c5baaad8badc8e7ae97bd8a582e3b4
blckct13 <blckct13@gmail.com>
2020-06-17 23:38:33 +0900

    app.pyを作成しました。

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import sqlite3
+from flask import Flask , render_template , request , redirect , session
+app = Flask(__name__)
+
+app.secret_key = 'NightTeamE'
+
+from datetime import datetime
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
